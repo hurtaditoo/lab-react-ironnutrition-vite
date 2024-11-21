df16b91a921a2e77bd11d1cc9efc15c847e44c3c
hurtaditoo <al415507@uji.es>
2024-11-21 21:12:40 +0100

    Iteration 2 | FoodBox component

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,23 @@
-// Your code here
+
+const FoodBox = ({ food, onDelete }) => {
+  const { name, calories, image, servings } = food;
+
+  return (
+    <div>
+        <p><b>{name}</b></p>
+
+        <img src={image} alt="Fruit Image" />
+
+        <p>Calories: {calories}</p>
+        <p>Servings {servings}</p>
+
+        <p>
+            <b>Total Calories: {calories * servings} </b> kcal
+        </p>
+
+        <button>Delete</button>
+    </div>
+  );
+};
+
+export default FoodBox;
