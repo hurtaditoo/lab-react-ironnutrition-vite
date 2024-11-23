3c8d8ea9edfe3e9e27f1c1c8b8bc9636fdfbb95a
hurtaditoo <al415507@uji.es>
2024-11-23 11:53:01 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import "./App.css";
-import foodsJson from "./foods.json";
 import { useState } from "react";
+import "./App.css";
+
+import foodsJson from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
-  const [foods, setFood] = useState(foodsJson);
+  const [foods, setFoods] = useState(foodsJson);
 
-  const deleteFood = idOfFoodToDelete =>
-		setFood(prevFood => prevFood.filter(food => food.id !== idOfFoodToDelete));
+  const onDeleteFood = (food) => {
+    const foodsWithoutFood = foods.filter((f) => f.id !== food.id);
+    setFoods(foodsWithoutFood);
+  };
+
+  const onAddFood = (food) => {
+    setFoods([food, ...foods]);
+  };
+
+  const foodBoxes = foods.map((food) => (
+    <FoodBox key={food.id} food={food} onDeleteFood={onDeleteFood} />
+  ));
 
   return (
     <div className="App">
-      <h2>Food List</h2>
+      <h1>LAB | React IronNutrition</h1>
 
-      <div className="food-list">
-        {foods.map((food, index) => (
-          <FoodBox key={index} food={food} onDelete={deleteFood} />
-        ))}
-      </div>
-      
+      <AddFoodForm onAddFood={onAddFood} />
+
+      {foodBoxes}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,30 +1,31 @@
-
-const FoodBox = ({ food, onDelete }) => {
-  const { name, calories, image, servings } = food;
-
-  const handleDelete = () => onDelete(food.id);
-
+function FoodBox({ food, onDeleteFood }) {
   return (
-    <div 
-        style={{ 
-            border: '1px solid lightgrey', 
-            paddingTop: '1rem',
-        }} 
+    <div style={{ 
+      border: '1px solid lightgrey', 
+      paddingTop: '1rem',
+      }} 
     >
-        <h4><b>{name}</b></h4>
 
-        <img src={image} alt="Fruit Image" style={{ maxWidth: '250px' }} />
+      <p>{food.name}</p>
 
-        <p>Calories: {calories}</p>
-        <p>Servings {servings}</p>
+      <img src={food.image} />
 
-        <p>
-            <b>Total Calories: {calories * servings} </b> kcal
-        </p>
+      <p>Calories: {food.calories}</p>
+      <p>Servings {food.servings}</p>
 
-        <button onClick={handleDelete}>Delete</button>
+      <p>
+        <b>Total Calories: {food.calories * food.servings} </b> kcal
+      </p>
+
+      <button
+        onClick={() => {
+          onDeleteFood(food);
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
-};
+}
 
 export default FoodBox;
